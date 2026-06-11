3c7eb98f6a84c9c08c8bdf7841eadfaf9f0f710d
Arghadeep-Ghosh-AG <arghagh24@gmail.com>
2026-06-11 23:25:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import streamlit as st
+import joblib
+
+model = joblib.load("resume_classifier.pkl")
 
 st.title("Resume Screening System")
 
-resume = st.text_area("Paste Resume Text Here")
+resume_text = st.text_area(
+    "Paste Resume Text",
+    height=300
+)
 
-if st.button("Predict"):
-    st.write("Prediction will appear here.")
+if st.button("Predict Category"):
+
+    if resume_text:
+
+        prediction = model.predict(
+            [resume_text]
+        )[0]
+
+        st.success(
+            f"Predicted Category: {prediction}"
+        )
