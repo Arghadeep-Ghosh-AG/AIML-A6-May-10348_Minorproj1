c9f3eb1a18bdca516cee121f95a77ef20e6f2c35
Arghadeep-Ghosh-AG <arghagh24@gmail.com>
2026-06-11 23:19:36 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,57 @@
 import pandas as pd
+import re
+import joblib
 
-print("Resume Screening System")
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+", " ", text)
+    text = re.sub(r"[^a-zA-Z ]", " ", text)
+    text = re.sub(r"\s+", " ", text)
+    return text
+
 
 df = pd.read_excel("dataset/Resume.xls")
 
-print(df.head())
-print(df.columns)
-print(df.shape)
+df["Resume_str"] = df["Resume_str"].apply(clean_text)
+
+X = df["Resume_str"]
+y = df["Category"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+model = Pipeline([
+    (
+        "tfidf",
+        TfidfVectorizer(
+            stop_words="english",
+            max_features=10000
+        )
+    ),
+    (
+        "classifier",
+        LinearSVC()
+    )
+])
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
+
+joblib.dump(model, "resume_classifier.pkl")
+
+print("Model Saved Successfully")
